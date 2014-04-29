5cd99c628f335c3dc0029a987607208da3a0f650
Florent Lamoureux <flrent@me.com>
2014-04-29 13:13:03 +0200

    oAuth2 moved to background

--- app/background.js
+++ app/background.js
@@ -3,11 +3,44 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     switch(request.action) {
 
       case 'oauth2.begin':
-        oauth2.begin();
+        var user;
+        console.log(sender);
+        chrome.storage.local.get('user', function(value) {
+        if(value.user === undefined || value.user.token === undefined) {
+            chrome.identity.launchWebAuthFlow({
+              'url': 'https://jawbone.com/auth/oauth2/auth?response_type=code&client_id=WtzakOFOnN4&redirect_uri='+chrome.identity.getRedirectURL()+'&scope=extended_readsleep_read',
+              'interactive': true
+            }, function(url) { 
+               var token = url.slice(parseInt(url.indexOf("?code="), 10)+6, url.length);
+               
+               $.getJSON('https://jawbone.com/nudge/api/users/@me/', {access_token: token})
+                .success( function(json) {
+                  var user = json.data;
+                    user.token = token;
+
+                  chrome.storage.local.set({'user': json.data}, function() {
+                    chrome.storage.local.get('user', function(value) {
+                      console.log(value);
+                    });
+                    sendResponse(user);
+                  });
+                })
+                .error( function(json) {
+                  alert("There has been an error.");
+                  console.log(json);
+                });
+            });
+          }
+          else {
+            user = value.user;
+            sendResponse(user);
+          }
+        });
+
         break;
 
       case 'oauth2.codeReceived':
-        oauth2.requestToken(request.code);
+        // oauth2.requestToken(request.code);
         break;
 
       default:
@@ -15,7 +48,6 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
         console.log(request);
         break;
     };
-    // sendResponse();
 
     return true;
   });
--- app/popup.js
+++ app/popup.js
@@ -60,38 +60,10 @@ var Reporter = (function () {
 
 
     var _authenticate = function(callback) {
-      chrome.storage.local.get('user', function(value) {
-        if(value.user === undefined || value.user.token === undefined) {
-          chrome.identity.launchWebAuthFlow({
-            'url': 'https://jawbone.com/auth/oauth2/auth?response_type=code&client_id=WtzakOFOnN4&redirect_uri='+chrome.identity.getRedirectURL()+'&scope=extended_readsleep_read',
-            'interactive': true
-          }, function(url) { 
-             var token = url.slice(parseInt(url.indexOf("?code="), 10)+6, url.length);
-             
-             $.getJSON('https://jawbone.com/nudge/api/users/@me/', {access_token: token})
-              .success( function(json) {
-                var user = json.data;
-                  user.token = token;
+      chrome.runtime.sendMessage({action:'oauth2.begin'}, function(response) {
 
-                chrome.storage.local.set({'user': json.data}, function() {
-                  chrome.storage.local.get('user', function(value) {
-                    console.log(value);
-                  });
-                  callback(user);
-                });
-              })
-              .error( function(json) {
-                alert("There has been an error.");
-                console.log(json);
-              });
-          });
-        }
-        else {
-          user = value.user;
-          callback(user);
-        }
+        callback(response);
       });
-      
     };
 
     return {
