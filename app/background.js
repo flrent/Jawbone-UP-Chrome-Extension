4b9571486d226c815514ad039d506cc00b97ea3c
Florent Lamoureux <flrent@me.com>
2014-08-23 22:04:23 +0200

    Various code update

--- app/background.js
+++ app/background.js
@@ -8,29 +8,37 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
         chrome.storage.local.get('user', function(value) {
         if(value.user === undefined || value.user.token === undefined) {
             console.log(chrome.identity.getRedirectURL());
+            var url = 'https://jawbone.com/auth/oauth2/auth?response_type=code&client_id=QVOCChMnoV4&redirect_uri=https://eikhkicddhikflbjolanffmmgniopohe.chromiumapp.org/&scope=extended_readsleep_read';
+            console.log(url);
             chrome.identity.launchWebAuthFlow({
-              'url': 'https://jawbone.com/auth/oauth2/auth?response_type=code&client_id=WtzakOFOnN4&redirect_uri=https://imjkegdfgajgdbgeondmlgddalgeefij.chromiumapp.org/&scope=extended_readsleep_read',
+              'url': url,
               'interactive': true
             }, function(url) { 
-              console.log(url);
-               var token = url.slice(parseInt(url.indexOf("?code="), 10)+6, url.length);
-               
-               $.getJSON('https://jawbone.com/nudge/api/users/@me/', {access_token: token})
-                .success( function(json) {
-                  var user = json.data;
-                    user.token = token;
+              if(chrome.runtime.lastError) {
+                alert(chrome.runtime.lastError.message);
+              }
+              else {
+                console.log(url);
+                 var token = url.slice(parseInt(url.indexOf("?code="), 10)+6, url.length);
+                 
+                 $.getJSON('https://jawbone.com/nudge/api/users/@me/', {access_token: token})
+                  .success( function(json) {
+                    var user = json.data;
+                      user.token = token;
 
-                  chrome.storage.local.set({'user': json.data}, function() {
-                    chrome.storage.local.get('user', function(value) {
-                      console.log(value);
+                    chrome.storage.local.set({'user': json.data}, function() {
+                      chrome.storage.local.get('user', function(value) {
+                        console.log(value);
+                      });
+                      sendResponse(user);
                     });
-                    sendResponse(user);
+                  })
+                  .error( function(json) {
+                    alert("There has been an error.");
+                    console.log(json);
                   });
-                })
-                .error( function(json) {
-                  alert("There has been an error.");
-                  console.log(json);
-                });
+              }
+              
             });
           }
           else {
