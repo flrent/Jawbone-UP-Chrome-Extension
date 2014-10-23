6d43989342dbea0c66eafe4446c8a3d71cfac653
flrent <flrent@me.com>
2014-10-22 23:37:01 -0700

    add charts

--- app/scripts/app/controllers/application.js
+++ app/scripts/app/controllers/application.js
@@ -1,4 +1,4 @@
-angular.module('jawbone.controllers')
+angular.module('jawbone.controllers', ['angularCharts'])
 .controller('ApplicationController',
   ['Storage', 'Api', '$scope', '$location',
     function(Storage, Api, $scope, $location) {
@@ -17,6 +17,39 @@ angular.module('jawbone.controllers')
   $scope.user = {};
 
   console.log('authenticating');
+
+$scope.config = {
+    title: 'Products',
+    tooltips: true,
+    labels: false,
+    mouseover: function() {},
+    mouseout: function() {},
+    click: function() {},
+    legend: {
+      display: true,
+      //could be 'left, right'
+      position: 'right'
+    }
+  };
+
+  $scope.data = {
+    series: ['Sales', 'Income', 'Expense', 'Laptops', 'Keyboards'],
+    data: [{
+      x: "Laptops",
+      y: [100, 500, 0],
+      tooltip: "this is tooltip"
+    }, {
+      x: "Desktops",
+      y: [300, 100, 100]
+    }, {
+      x: "Mobiles",
+      y: [351]
+    }, {
+      x: "Tablets",
+      y: [54, 0, 879]
+    }]
+  };
+
 
   API.authenticate(function(newToken) {
     token = newToken;
